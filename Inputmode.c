82493b1ad463df40133f7cf4582cd0bacc6631a2
LuckyG0ldfish <tim-koeneke@online.de>
2022-10-30 19:01:10 +0100

    mode split

---
+++ Inputmode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool TestFile() {
+    return true; 
+}
+
+bool RunInputMode() {
+
+
+    return true; 
+}
--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "Inputmode.c"
+#include "QuestionMode.c"
+
+bool getInput(); 
+bool RunInputMode(); 
+bool selectMode(int i);
+
+char input[20];
+char output[20];  
+char solution[20]; 
+bool correct; 
+const int InputMode = 1; 
+const int QuestionMode = 2; 
+
 
 void main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+    printf("Hello, World!\n");
+    getInput(); 
+    printf("%s\n", input); 
+    if (TestFile()) {
+        printf("läuft"); 
+    } else {
+        printf("was is File?"); 
+    }
+}
+
+bool selectMode(int i) {
+    if (i == InputMode) {
+        RunInputMode(); 
+    } else if (i == QuestionMode) {
+        RunQuestionMode(); 
+    } else {
+        printf("Only '1' and '2' are viable inputs"); 
+    }
+}
+
+bool getInput() {
+    /*where store, length, stdin = standart input*/
+    fgets(input, 20, stdin);
+    return true; 
+}
+
+bool checkResponse() {
+    
 }
 
